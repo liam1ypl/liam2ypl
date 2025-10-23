f5a068e49006db15f80bd8e567ce633719a64e32
liam1ypl <647363360@qq.com>
2025-10-23 11:06:45 +0800

    1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,14 @@
-//
-//
-//
+//202510304214
+//647363360@qq.com
+//杨攀亮
 #include <stdio.h>
 int main()
 {
-    printf("Hello World");
+    float celsius=0;
+    printf("请输入摄氏温度：");
+    scanf("%f",&celsius);
+    float fahrenheit =celsius*9/5+32;
+    printf("华氏温度为%f。\n",fahrenheit);
     return 0;
+
 }
