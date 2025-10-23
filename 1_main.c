3b6245d4c6721eb005abcba444e20f449d7f2031
liam1ypl <647363360@qq.com>
2025-10-23 11:47:29 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -4,11 +4,12 @@
 #include <stdio.h>
 int main()
 {
-    float celsius=0;
-    printf("请输入摄氏温度：");
-    scanf("%f",&celsius);
-    float fahrenheit =celsius*9/5+32;
-    printf("华氏温度为%f。\n",fahrenheit);
+    float celsius;
+    printf("获取用户输入的摄氏温度:");
+    scanf("%f", &celsius);
+    float fahrenheit = celsius * 9.0 / 5.0 + 32;
+    printf("%.1f\n", fahrenheit);
     return 0;
+
 
 }
