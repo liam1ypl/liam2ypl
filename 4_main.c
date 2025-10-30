ad54e7f5b00d879a2e8f0e64b4fb4faef0f07c34
liam1ypl <647363360@qq.com>
2025-10-30 21:26:44 +0800

    Add files via upload

---
+++ 4_main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num = 100; 
+    int digit1, digit2, digit3;
+    int first = 1; 
+    while (num <= 999) {
+        digit1 = num / 100;
+        digit2 = (num / 10) % 10;
+        digit3 = num % 10;
+        if (digit1*digit1*digit1 + digit2*digit2*digit2 + digit3*digit3*digit3 == num) {
+            if (first) {
+                printf("%d", num);
+                first = 0;         
+            } else {
+                printf(" %d", num); 
+            }
+        }
+
+        num++; 
+    }
+
+    return 0;
+}
---
+++ 6_main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int arr[5];
+    int count=0;
+    int num;
+    while(count<5){
+        scanf("%d",&num);
+        if(num % 2 ==0){
+            arr[count]=num;
+            count++;
+        }
+    }
+    for(int i=0;i<5;i++){
+        if(i==0){
+            printf("%d",arr[i]);
+        }else{
+            printf(" %d",arr[i]);
+        }
+    }
+    return 0;
+
+
+}
