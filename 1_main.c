94e8dd49fdbc2836d60ef47800d2f4a3f2c84a5e
liam1ypl <647363360@qq.com>
2025-10-31 09:05:01 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,26 +1,32 @@
-//202510304214
+ //202510304214
 //647363360@qq.com
 //杨攀亮
 #include <stdio.h>
 #include <stdbool.h>
 bool isPrime(int n)
 {
-    if(n<2){
+    if(n <=1){
+        return false;
+    }if(n == 2){
+        return true;
+    }if(n % 2==0){
         return false;
     }
-    for(int i=2;i * i <= n; i++){
-        if(n % i ==0){
+    for(int i=3;i * i <=n;i+=2){
+        if(n % i==0){
             return false;
         }
     }
     return true;
+
+
 }
 int main()
 {
     int n;
     printf("请输入一个小于50的正整数:");
     scanf("%d",&n);
-    if (n<=0 || n>=50){
+    if (n<1  || n>=50){
         printf("请输入一个小于50的正整数");
         return 0;
     }
