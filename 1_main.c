77900e1887120581c507a75f4f5624bc3091ea6f
liam1ypl <647363360@qq.com>
2025-10-31 13:25:59 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -3,37 +3,37 @@
 //杨攀亮
 #include <stdio.h>
 #include <stdbool.h>
-bool isPrime(int n)
-{
-    if(n <=1){
-        return false;
-    }if(n == 2){
-        return true;
-    }if(n % 2==0){
+bool isPrime(int num) {
+    if (num <= 1) {
         return false;
     }
-    for(int i=3;i * i <=n;i+=2){
-        if(n % i==0){
+    if (num == 2) {
+        return true;
+    }
+    if (num % 2 == 0) {
+        return false;
+    }
+    for (int i = 3; i * i <= num; i += 2) {
+        if (num % i == 0) {
             return false;
         }
     }
     return true;
 }
-int main()
-{
+
+int main() {
     int n;
-    printf("请输入一个小于50的正整数:");
-    scanf("%d",&n);
-    if (n<1  || n>=50){
-        printf("请输入一个小于50的正整数");
-        return 0;
+    printf("请输入一个小于50的正整数作为密钥：");
+    scanf("%d", &n);
+    if (n <= 0 || n >= 50) {
+        printf("输入不符合要求，请输入一个大于0且小于50的正整数。\n");
+        return 1; 
     }
-    if(isPrime(n)){
-        printf("密钥正确，密码设置成功\n");
-    } else{
+    if (isPrime(n)) {
+        printf("密钥安全，密码设置成功\n");
+    } else {
         printf("密钥不安全，请重新输入\n");
     }
+
     return 0;
-
 }
-
