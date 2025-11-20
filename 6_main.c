15ae5b809ab4791bf22b9099d7cedc866ccdace5
liam1ypl <647363360@qq.com>
2025-11-20 21:13:14 +0800

    Update 6_main.c

--- 6_main.c
+++ 6_main.c
@@ -1,29 +1,18 @@
 //202510304214
 //647363360@qq.com
 //杨攀亮
-#include<stdio.h>
-int calculateSum(int arr[],int length){
-    int sum=0;
-    for(int i= 0;i< length;i++){
-    sum +=arr[i];
+#include <stdio.h>
+#include <stdlib.h> 
+int main() {
+    int *arr = (int *)malloc(5 * sizeof(int)); 
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", arr + i);
     }
-    return sum;
-}
-int calculateProduct(int arr[],int length){
-    int product=1;
-    for(int i =0;i<length;i++){
-        product *=arr[i];
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", *(arr + i));
     }
-    return product;
-}
-int main()
-{
-    int arr[5];
-    for(int i = 0;i < 5;i++){
-        scanf("%d",&arr[i]);
-    }
-    int sum =calculateSum(arr,5);
-    int product =calculateProduct(arr,5);
-    printf("%d %d\n",sum,product);
+    printf("\n");
+    free(arr); 
+    arr = NULL;  
     return 0;
 }
