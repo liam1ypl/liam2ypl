9e6c95638c04b16f4abb65c7e1a8f97d35430e8e
liam1ypl <647363360@qq.com>
2025-11-20 21:10:11 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -2,29 +2,17 @@
 //647363360@qq.com
 //杨攀亮
 #include <stdio.h>
-int main() {
-    int arr[10];
-    for (int i = 0; i < 10; i++) {
-        scanf("%d", &arr[i]);
-    }
-    for (int i = 0; i < 10 - 1; i++) {
-        for (int j = 0; j < 10 - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
-               
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    for (int i = 0; i < 10; i++) {
-        if (i == 9) {
-            printf("%d", arr[i]);
-        } else {
-            printf("%d ", arr[i]);
-        }
-    }
-    printf("\n");
 
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    swap(&x, &y);
+    printf("%d %d\n", x, y);
     return 0;
 }
