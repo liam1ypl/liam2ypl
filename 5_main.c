d813cfbe6619a8f9561b375b0dceea8eba7f8594
liam1ypl <647363360@qq.com>
2025-11-20 21:11:52 +0800

    Update 5_main.c

--- 5_main.c
+++ 5_main.c
@@ -1,15 +1,26 @@
 //202510304214
 //647363360@qq.com
 //杨攀亮
-#include<stdio.h>
-int arithmeticSum(int a1,int an,int step)
-{
-    int n=(an-a1)/step + 1;
-    return n*(a1 + an) / 2;
+#include <stdio.h>
+
+void shiftArray(int *ptr_arr, int len) {
+    for (int i = len - 1; i > 0; i--) {
+        *(ptr_arr + i) = *(ptr_arr + i - 1);
+    }
+    *ptr_arr = 0;  
 }
-int main()
-{
-    int result =arithmeticSum(1,100,1);
-    printf("%d\n",result);
+
+int main() {
+    int arr[5];
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+    shiftArray(arr, 5);
+    for (int i = 0; i < 5; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
     return 0;
 }
+ 
+ 
